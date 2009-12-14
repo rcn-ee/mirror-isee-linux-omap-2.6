f7190bd32dc5ece9a4a8337dafe3f9e63411bc5e
Enric Balletbo i Serra <eballetbo@iseebcn.com>
2009-12-14 13:25:48 +0100

    [ARM] igep0020: cleanup code and change flash partitions due to DDP support in first and second bootloader
    Creating 5 MTD partitions on "omap2-onenand":
       0x00000000-0x00080000 : "X-Loader"
       0x00080000-0x00200000 : "U-Boot"
       0x00200000-0x00280000 : "U-Boot Env"
       0x00280000-0x00580000 : "Kernel"
       0x00580000-0x20000000 : "File System"

--- arch/arm/mach-omap2/board-igep0020-flash.c
+++ arch/arm/mach-omap2/board-igep0020-flash.c
@@ -24,43 +24,46 @@
 #include <linux/platform_device.h>
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/partitions.h>
-#include <linux/mtd/nand.h>
 #include <linux/mtd/onenand_regs.h>
 #include <linux/types.h>
 #include <linux/io.h>
 
-#include <asm/mach/flash.h>
-
 #include <mach/board-igep0020.h>
 #include <mach/board.h>
 #include <mach/gpmc.h>
-#include <mach/nand.h>
 #include <mach/onenand.h>
 
-static int igep2_onenand_setup(void __iomem *, int freq);
+#if defined(CONFIG_MTD_ONENAND_OMAP2) || \
+         defined(CONFIG_MTD_ONENAND_OMAP2_MODULE)
+
+/* NAND04GR4E1A ( x2 Flash built-in COMBO POP MEMORY )
+ * Since the device is equipped with two DataRAMs, and two-plane NAND
+ * Flash memory array, these two component enables simultaneous program
+ * of 4KiB. Plane1 has only even blocks such as block0, block2, block4
+ * while Plane2 has only odd blocks such as block1, block3, block5.
+ * So MTD regards it as 4KiB page size and 256KiB block size 64*(2*2048)
+ */
 
 static struct mtd_partition igep2_onenand_partitions[] = {
 	{
 		.name           = "X-Loader",
 		.offset         = 0,
-		.size           = 4*(64*2048),
-		.mask_flags     = MTD_WRITEABLE
+		.size           = 2 * (64*(2*2048))
 	},
 	{
 		.name           = "U-Boot",
 		.offset         = MTDPART_OFS_APPEND,
-		.size           =  15*(64*2048),
-		.mask_flags     = MTD_WRITEABLE
+		.size           = 6 * (64*(2*2048)),
 	},
 	{
 		.name           = "U-Boot Env",
 		.offset         = MTDPART_OFS_APPEND,
-		.size           = 1*(64*2048),
+		.size           = 2 * (64*(2*2048)),
 	},
 	{
 		.name           = "Kernel",
 		.offset         = MTDPART_OFS_APPEND,
-		.size           = 40*(64*2048),
+		.size           = 12 * (64*(2*2048)),
 	},
 	{
 		.name           = "File System",
@@ -68,6 +71,12 @@ static struct mtd_partition igep2_onenand_partitions[] = {
 		.size           = MTDPART_SIZ_FULL,
 	},
 };
+
+static int igep2_onenand_setup(void __iomem *onenand_base, int freq)
+{
+       /* nothing is required to be setup for onenand as of now */
+       return 0;
+}
 
 static struct omap_onenand_platform_data igep2_onenand_data = {
 	.parts = igep2_onenand_partitions,
@@ -84,77 +93,10 @@ static struct platform_device igep2_onenand_device = {
 	},
 };
 
-static struct mtd_partition igep2_nand_partitions[] = {
-	/* All the partition sizes are listed in terms of NAND block size */
-	{
-		.name		= "xloader-nand",
-		.offset		= 0,
-		.size		= 4*(128 * 1024),
-		.mask_flags	= MTD_WRITEABLE
-	},
-	{
-		.name		= "uboot-nand",
-		.offset		= MTDPART_OFS_APPEND,
-		.size		= 14*(128 * 1024),
-		.mask_flags	= MTD_WRITEABLE
-	},
-	{
-		.name		= "params-nand",
-
-		.offset		= MTDPART_OFS_APPEND,
-		.size		= 2*(128 * 1024)
-	},
-	{
-		.name		= "linux-nand",
-		.offset		= MTDPART_OFS_APPEND,
-		.size		= 40*(128 * 1024)
-	},
-	{
-		.name		= "jffs2-nand",
-		.size		= MTDPART_SIZ_FULL,
-		.offset		= MTDPART_OFS_APPEND,
-	},
-};
-
-static struct omap_nand_platform_data igep2_nand_data = {
-	.parts		= igep2_nand_partitions,
-	.nr_parts	= ARRAY_SIZE(igep2_nand_partitions),
-	.nand_setup	= NULL,
-	.dma_channel	= -1,		/* disable DMA in OMAP NAND driver */
-	.dev_ready	= NULL,
-};
-
-static struct resource igep2_nand_resource = {
-	.flags		= IORESOURCE_MEM,
-};
-
-static struct platform_device igep2_nand_device = {
-	.name		= "omap2-nand",
-	.id		= 0,
-	.dev		= {
-		.platform_data	= &igep2_nand_data,
-	},
-	.num_resources	= 1,
-	.resource	= &igep2_nand_resource,
-};
-
-/*
- *      igep2_onenand_setup - Set the onenand sync mode
- *      @onenand_base:  The onenand base address in GPMC memory map
- *
- */
-
-static int igep2_onenand_setup(void __iomem *onenand_base, int freq)
-{
-	/* nothing is required to be setup for onenand as of now */
-	return 0;
-}
-
 void __init igep2_flash_init(void)
 {
 	u8		cs = 0;
-	u8		onenandcs = GPMC_CS_NUM + 1, nandcs = GPMC_CS_NUM + 1;
-	u32		gpmc_base_add = OMAP34XX_GPMC_VIRT;
+	u8		onenandcs = GPMC_CS_NUM + 1;
 
 	while (cs < GPMC_CS_NUM) {
 		u32 ret = 0;
@@ -168,31 +110,19 @@ void __init igep2_flash_init(void)
 		* lines of machineID. Check if NAND/oneNAND is configured
 		*/
 		if ((ret & 0xC00) == 0x800) {
-			/* Found it!! */
-			if (nandcs > GPMC_CS_NUM)
-				nandcs = cs;
+			/* NAND found */
 		} else {
 			ret = gpmc_cs_read_reg(cs, GPMC_CS_CONFIG7);
 			if ((ret & 0x3F) == (ONENAND_MAP >> 24))
+				/* ONENAND found */
 				onenandcs = cs;
 		}
 		cs++;
 	}
-	if ((nandcs > GPMC_CS_NUM) && (onenandcs > GPMC_CS_NUM)) {
-		printk(KERN_INFO "NAND/OneNAND: Unable to find configuration "
+	if ( (onenandcs > GPMC_CS_NUM)) {
+		printk(KERN_INFO "OneNAND: Unable to find configuration "
 				" in GPMC\n ");
 		return;
-	}
-
-	if (nandcs < GPMC_CS_NUM) {
-		igep2_nand_data.cs	= nandcs;
-		igep2_nand_data.gpmc_cs_baseaddr = (void *)(gpmc_base_add +
-					GPMC_CS0_BASE + nandcs*GPMC_CS_SIZE);
-		igep2_nand_data.gpmc_baseaddr   = (void *) (gpmc_base_add);
-
-		if (platform_device_register(&igep2_nand_device) < 0) {
-			printk(KERN_ERR "Unable to register NAND device\n");
-		}
 	}
 
 	if (onenandcs < GPMC_CS_NUM) {
@@ -202,3 +132,8 @@ void __init igep2_flash_init(void)
 	}
 }
 
+#else
+
+void __init igep2_flash_init(void) {}
+
+#endif
