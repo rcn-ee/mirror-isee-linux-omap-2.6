d2394fff46ccd1512777b967780c6864893a8f16
Enric Balletbo i Serra <eballetbo@iseebcn.com>
2010-02-26 19:02:16 +0100

    video/modedb.c: Add timmings for CEA 681-E Format 4 and 19
    Support:
      -  1280x720 @ 60 Hz, 45 kHz hsync, CEA 681-E Format 4
      -  1280x720 @ 50 Hz, 45 kHz hsync, CEA 681-E Format 19

--- drivers/video/modedb.c
+++ drivers/video/modedb.c
@@ -45,6 +45,14 @@ static const struct fb_videomode modedb[] = {
 	NULL, 60, 640, 480, 39721, 40, 24, 32, 11, 96, 2,
 	0, FB_VMODE_NONINTERLACED
     }, {
+	/* 1280x720 @ 60 Hz, 45 kHz hsync, CEA 681-E Format 4 */
+	"hd720", 60, 1280, 720, 13468, 110, 220, 5, 20, 40, 5,
+	0, FB_VMODE_NONINTERLACED
+    }, {
+	/* 1280x720 @ 50 Hz, 45 kHz hsync, CEA 681-E Format 19 */
+	"hd720", 50, 1280, 720, 13468, 440, 220, 5, 20, 40, 5,
+	0, FB_VMODE_NONINTERLACED
+    }, {
 	/* 800x600 @ 56 Hz, 35.15 kHz hsync */
 	NULL, 56, 800, 600, 27777, 128, 24, 22, 1, 72, 2,
 	0, FB_VMODE_NONINTERLACED
