1747de88baf591b1155450928485d1bfb1c31925
Enric Balletbo i Serra <eballetbo@iseebcn.com>
2011-04-05 16:15:14 +0200

    OMAP3: igep0030: Change machine name to real name
    Signed-off-by: Enric Balletbo i Serra <eballetbo@iseebcn.com>

--- arch/arm/mach-omap2/board-igep0030.c
+++ arch/arm/mach-omap2/board-igep0030.c
@@ -458,7 +458,7 @@ static void __init igep3_init(void)
 	igep3_init_led();
 }
 
-MACHINE_START(IGEP0030, "IGEP OMAP3 module")
+MACHINE_START(IGEP0030, "IGEP 0030")
 	.phys_io	= 0x48000000,
 	.io_pg_offst	= ((0xfa000000) >> 18) & 0xfffc,
 	.boot_params	= 0x80000100,
