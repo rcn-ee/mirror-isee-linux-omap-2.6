6a9db169f2c7ef5a3626725608ae02f450655371
Enric Balletbo i Serra <eballetbo@iseebcn.com>
2011-06-16 16:19:34 +0200

    OMAP3: Move common twl configuration to twl-common
    X-Patchwork-Id: 875672
    
    Reduce the amount of duplicated code by moving the common
    configuration for twl4030/5030/tpsxx to the twl-common file.
    Use the omap3_pmic_get_config function from board files to
    properly configure the PMIC with the common fields.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- arch/arm/mach-omap2/twl-common.h
+++ arch/arm/mach-omap2/twl-common.h
@@ -1,6 +1,11 @@
 #ifndef __OMAP_PMIC_COMMON__
 #define __OMAP_PMIC_COMMON__
 
+#define TWL_COMMON_PDATA_USB		(1 << 0)
+#define TWL_COMMON_PDATA_BCI		(1 << 1)
+#define TWL_COMMON_PDATA_MADC		(1 << 2)
+#define TWL_COMMON_PDATA_AUDIO		(1 << 3)
+
 struct twl4030_platform_data;
 
 void omap_pmic_init(int bus, u32 clkrate, const char *pmic_type, int pmic_irq,
@@ -24,5 +29,8 @@ static inline void omap4_pmic_init(const char *pmic_type,
 	/* Phoenix Audio IC needs I2C1 to start with 400 KHz or less */
 	omap_pmic_init(1, 400, pmic_type, OMAP44XX_IRQ_SYS_1N, pmic_data);
 }
+
+void omap3_pmic_get_config(struct twl4030_platform_data *pmic_data,
+			   u32 pdata_flags, u32 regulators_flags);
 
 #endif /* __OMAP_PMIC_COMMON__ */
