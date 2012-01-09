3d809fd932bb9e2a5c4b94abcde1a92c3f8c023d
Javier Martinez Canillas <javier@dowhile0.org>
2012-01-09 11:28:03 +0100

    net/smsc911x: Remove energy detect conditionally compilation
    The reset function guards the energy detect enable/disable code
    execution using the SMSC LAN chips generation 4. So, disabling
    the energy code when CONFIG_SMSC_PHY is not set and the generic
    phy driver is used is not necessary.

--- drivers/net/smsc911x.c
+++ drivers/net/smsc911x.c
@@ -1124,7 +1124,6 @@ static void smsc911x_rx_multicast_update_workaround(struct smsc911x_data *pdata)
 	spin_unlock(&pdata->mac_lock);
 }
 
-#ifdef CONFIG_SMSC_PHY
 static int smsc911x_phy_disable_energy_detect(struct smsc911x_data *pdata)
 {
 	int rc = 0;
@@ -1190,17 +1189,6 @@ static int smsc911x_phy_enable_energy_detect(struct smsc911x_data *pdata)
 	}
 	return 0;
 }
-#else
-static inline int smsc911x_phy_disable_energy_detect(struct smsc911x_data *pdata)
-{
-	return 0;
-}
-
-static inline int smsc911x_phy_enable_energy_detect(struct smsc911x_data *pdata)
-{
-	return 0;
-}
-#endif
 
 static int smsc911x_soft_reset(struct smsc911x_data *pdata)
 {
