5d6c9acc22f84e0f4c4e3a731e591256a4374ec3
Matthias Brugger <mbrugger@iseebcn.com>
2012-05-11 17:32:05 +0200

    net/smsc911x: Delete faulty comment
    Signed-off-by: Matthias Brugger <mbrugger@iseebcn.com>

--- drivers/net/smsc911x.c
+++ drivers/net/smsc911x.c
@@ -2225,8 +2225,6 @@ out_0:
 /* This implementation assumes the devices remains powered on its VDDVARIO
  * pins during suspend. */
 
- * pins during suspend. */
-
 /* TODO: implement freeze/thaw callbacks for hibernation.*/
 
 static int smsc911x_suspend(struct device *dev)
