04a083a618d16db47c476e66ec7dea63d7308d87
Agusti Fontquerni <afontquerni@iseebcn.com>
2015-03-24 13:36:45 +0100

    handle wl8 CCCR revision

--- drivers/mmc/core/sdio.c
+++ drivers/mmc/core/sdio.c
@@ -105,12 +105,14 @@ static int sdio_read_cccr(struct mmc_card *card)
 
 	cccr_vsn = data & 0x0f;
 
+#if 0
+// WL18xx returns CCR_REV_2_00 and so this is not an error
 	if (cccr_vsn > SDIO_CCCR_REV_1_20) {
 		printk(KERN_ERR "%s: unrecognised CCCR structure version %d\n",
 			mmc_hostname(card->host), cccr_vsn);
 		return -EINVAL;
 	}
-
+#endif
 	card->cccr.sdio_vsn = (data & 0xf0) >> 4;
 
 	ret = mmc_io_rw_direct(card, 0, 0, SDIO_CCCR_CAPS, 0, &data);
