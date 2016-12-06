173cb66fa19074b3e2bf653121cf0279b02bddc2
Josep Orga <jorga@iseebcn.com>
2016-12-06 09:54:16 +0100

    igep0020: Add mmc regulator for wilink8 reset
    Signed-off-by: Josep Orga <jorga@iseebcn.com>
    Signed-off-by: Agusti Fontquerni <afontquerni@iseebcn.com>

--- arch/arm/mach-omap2/board-igep0020.c
+++ arch/arm/mach-omap2/board-igep0020.c
@@ -23,6 +23,7 @@
 #include <linux/mmc/host.h>
 
 #include <linux/spi/spi.h>
+#include <linux/regulator/fixed.h>
 #include <linux/regulator/machine.h>
 #include <linux/regulator/fixed.h>
 #include <linux/i2c/twl.h>
@@ -191,8 +192,37 @@ static struct omap2_hsmmc_info mmc[] = {
 };
 
 #if defined(CONFIG_WL12XX_PLATFORM_DATA) && !defined(CONFIG_LIBERTAS_SDIO_MODULE)
-static struct wl12xx_platform_data wl12xx __initdata;
 
+static struct regulator_consumer_supply igep0020_vmmc2_supply =
+	REGULATOR_SUPPLY("vmmc_aux", "mmci-omap-hs.1");
+
+static struct regulator_init_data igep0020_vmmc2 = {
+	.constraints = {
+		.valid_ops_mask = REGULATOR_CHANGE_STATUS,
+	},
+	.num_consumer_supplies = 1,
+	.consumer_supplies = &igep0020_vmmc2_supply,
+};
+
+static struct fixed_voltage_config igep0020_vwlan = {
+	.supply_name = "vwl1271",
+	.microvolts = 1800000,  // 1.8V
+	.gpio = IGEP2_RF_GPIO_WL_EN,
+	.startup_delay = 70000, // 70ms
+	.enable_high = 1,
+	.enabled_at_boot = 0,				//Enabled @ boot
+	.init_data = &igep0020_vmmc2,
+};
+
+static struct platform_device omap_vwlan_device = {
+	.name		= "reg-fixed-voltage",
+	.id		= 1,
+	.dev = {
+		.platform_data = &igep0020_vwlan,
+	},
+};
+
+static struct wl12xx_platform_data wl12xx __initdata;
 static void __init __used legacy_init_wl12xx(unsigned ref_clock,
                                             unsigned tcxo_clock,
                                             int gpio){
@@ -208,12 +238,13 @@ static void __init __used legacy_init_wl12xx(unsigned ref_clock,
 		pr_err("error setting wl12xx data: %d\n", res);
 		return;
 	}
+
+	platform_device_register(&omap_vwlan_device);
+
 }
 
-static void __init ti_wl12xx_wlan_init(void){
-	struct device *dev;
-	struct omap_mmc_platform_data *pdata;
-
+static void __init ti_wl12xx_wlan_init(void)
+{
 	/* Set up the WLAN_EN mux */
 	omap_mux_init_gpio(IGEP2_RF_GPIO_WL_EN, OMAP_PIN_OUTPUT);
 
@@ -228,6 +259,7 @@ static void __init ti_wl12xx_wlan_init(void){
 		gpio_set_value(IGEP2_RF_GPIO_WL_EN, 0);
 		udelay(10);
 		gpio_set_value(IGEP2_RF_GPIO_WL_EN, 1);
+		gpio_free(IGEP2_RF_GPIO_WL_EN);
 	} else
 		pr_warning("IGEP: Could not obtain gpio WL EN\n");
 
@@ -248,25 +280,6 @@ static void __init ti_wl12xx_wlan_init(void){
 	legacy_init_wl12xx(WL12XX_REFCLOCK_38_XTAL, WL12XX_TCXOCLOCK_26,
 		IGEP2_RF_GPIO_W_IRQ);
 
-	/*
-	* Set our set_power callback function which will be called from
-	* set_ios. This is requireq since, unlike other omap2+ platforms, a
-	* no-op set_power function is registered. Thus, we cannot use a fixed
-	* regulator, as it will never be toggled.
-	* Moreover, even if this was not the case, we're on mmc0, for which
-	* omap_hsmmc' set_power functions do not toggle any regulators.
-	*/
-	dev = mmc[1].dev;
-	if (!dev) {
-		pr_err("wl12xx mmc device initialization failed\n");
-		return;
-	}
-
-	pdata = dev->platform_data;
-	if (!pdata) {
-		pr_err("Platform data of wl12xx device not set\n");
-		return;
-	}
 }
 
 static void __init ti_wl12xx_init(void)
